3b98ef0c4b982c982883c800e14eee2c35339fca
Ahmed Abdelhamid <ahmedabdelhamid887@gmail.com>
2023-06-21 10:37:46 +0300

    generate type adapater and init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/screens/home_notes_screen.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('Notes Box');
   runApp(const MyApp());
 }
 
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,17 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+//this number in hive type is unique in general
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  //this number in hive field is unique into this class only
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel({
@@ -11,3 +21,4 @@ class NoteModel {
     required this.color,
   });
 }
+// ( flutter packages pub run build_runner build ) ---> this code write it in terminal to generate file of this file model.
